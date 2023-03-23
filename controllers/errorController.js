4263f9c1ccb09a4102fae3e95df70ac37bd90a34
Rawi Daniel <rawidaniel96@gmail.com>
2023-03-23 11:22:33 +0300

    error handler

---
+++ controllers/errorController.js
@@ -0,0 +1,83 @@
+const AppError = require('../utils/appError');
+
+const handleCastErrorDB = (err) => {
+  const message = `Invalid ${err.path}: ${err.value}`;
+  return new AppError(message, 400);
+};
+
+const handleDuplicateFieldsDB = (err) => {
+  console.log(err);
+  const value = err.keyValue.name;
+  const message = `Duplicate field value: "${value}". Please use another value`;
+  return new AppError(message, 400);
+};
+
+const handleValidationErrorDB = (err) => {
+  const value = Object.values(err.errors)
+    .map((ele) => ele.properties)
+    .map((ele) => ele.message);
+  const message = `Invalid input data. ${value.join('. ')}`;
+  return new AppError(message, 400);
+};
+
+const handleJwtTokenError = () =>
+  new AppError('Invald token, please log in again.', 401);
+
+const handleJwtExpiredError = () =>
+  new AppError('Your token has expired, please log in again.', 401);
+
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  } else {
+    console.log('Error 💥', err);
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went very wrong.',
+    });
+  }
+};
+
+module.exports = (err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+  // console.log(err.name);
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    let error = { ...err };
+    error.message = err.message;
+
+    if (err.name === 'CastError') {
+      error = handleCastErrorDB(error);
+    }
+
+    if (err.code === 11000) {
+      error = handleDuplicateFieldsDB(error);
+    }
+
+    if (err.name === 'ValidationError') {
+      error = handleValidationErrorDB(error);
+    }
+    if (err.name === 'JsonWebTokenError') {
+      error = handleJwtTokenError();
+    }
+    if (err.name === 'TokenExpiredError') {
+      error = handleJwtExpiredError();
+    }
+
+    sendErrorProd(error, res);
+  }
+};

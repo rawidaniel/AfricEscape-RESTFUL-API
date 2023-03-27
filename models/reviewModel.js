75b40e0f39633e741d2547e77729d30bd1f25706
Rawi Daniel <rawidaniel96@gmail.com>
2023-03-27 15:16:04 +0300

    populate tour document in review model

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -30,7 +30,10 @@ const reviewSchema = new mongoose.Schema(
 reviewSchema.index({ user: 1, tour: 1 }, { unique: true });
 
 reviewSchema.pre(/^find/, function (next) {
-  this.populate({ path: 'user', select: 'name photo' }).select('-__v');
+  this.populate({ path: 'user', select: 'name photo' }).populate({
+    path: 'tour',
+    select: 'name imageCover -guides',
+  });
   next();
 });
 
